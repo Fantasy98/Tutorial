8c3eb5eb96de9e628976fd5ae07a4402fe7dfbf6
yuning <yuningw@kth.se>
2022-10-30 06:59:29 +0100

    A test of git

---
+++ test.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+x= np.arange(10)
+y = 3*x + 1
+
+plt.plot(x,y)
+plt.show()
---
+++ test2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+x = np.arange(10)
+
+y = -3 * x +10
+
+plt.plot(x,y)
+plt.show()
