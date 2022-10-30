4520bfb1f69a5b3de670fda4698704eb56f8859a
yuning <yuningw@kth.se>
2022-10-30 07:29:05 +0100

    Changed test.py plots

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import numpy as np
 
 x= np.arange(10)
 y = 3*x + 1
-
-plt.plot(x,y)
+z = -4*x -10
+plt.plot(x,y,"r-.",label="Red line")
+plt.plot(x,z,"b-",label="Blue line")
+plt.legend()
 plt.show()
